7ed35ac5707337e12f9acd695f1f312a254361d6
andregustavo1 <andregustavo5261@gmail.com>
2024-01-03 20:59:33 -0300

    up

---
+++ uLinker/src/js/script.js
@@ -0,0 +1,42 @@
+const emailBtn = document.getElementById('emailBtn');
+const emailText = document.getElementById('emailText');
+const emailBtnText = document.getElementById('emailBtnText');
+
+emailBtn.addEventListener('click', () => {
+    navigator.clipboard.writeText(emailText.innerText);
+
+    emailBtnText.textContent = 'Copiado!';
+
+    setTimeout(() => {
+        emailBtnText.textContent = 'E-Mail';
+    }, 1200);
+});
+
+const container = document.getElementById('container');
+const btnPix = document.getElementById('btnPix');
+const pix = document.getElementById('pix');
+
+btnPix.addEventListener('click', () => {
+    container.classList.add('hidden');
+    pix.classList.remove('hidden');
+});
+
+const pixBack = document.getElementById('pixBack');
+
+pixBack.addEventListener('click', () => {
+    container.classList.remove('hidden');
+    pix.classList.add('hidden');
+});
+
+const pixBtn = document.getElementById('pixBtn');
+const pixKey = document.getElementById('pixKey');
+
+pixBtn.addEventListener('click', () => {
+    navigator.clipboard.writeText(pixKey.innerText);
+
+    pixBtn.textContent = 'Copiado!';
+
+    setTimeout(() => {
+        pixBtn.textContent = 'Copiar Chave Pix';
+    }, 1200);
+});
---
+++ uLinker/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html", "./profiles/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        lexend: ["Lexend Deca", "sans-serif"],
+      },
+
+      keyframes: {
+        blink: {
+        '0%': { opacity: '100%' },
+        '50%': { opacity: '0%' },
+        }
+      },
+
+      animation: {
+        blink: 'blink 1s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+}
+
+
