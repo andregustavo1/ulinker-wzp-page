81da3c36e9d060a9c4e854e6b8ed0683621c9541
andregustavo1 <andregustavo5261@gmail.com>
2024-01-17 21:15:48 -0300

    up

--- xlinker/src/js/script.js
+++ xlinker/src/js/script.js
@@ -1,42 +1,42 @@
-const emailBtn = document.getElementById('emailBtn');
-const emailText = document.getElementById('emailText');
-const emailBtnText = document.getElementById('emailBtnText');
+// const emailBtn = document.getElementById('emailBtn');
+// const emailText = document.getElementById('emailText');
+// const emailBtnText = document.getElementById('emailBtnText');
 
-emailBtn.addEventListener('click', () => {
-    navigator.clipboard.writeText(emailText.innerText);
+// emailBtn.addEventListener('click', () => {
+//     navigator.clipboard.writeText(emailText.innerText);
 
-    emailBtnText.textContent = 'Copiado!';
+//     emailBtnText.textContent = 'Copiado!';
 
-    setTimeout(() => {
-        emailBtnText.textContent = 'E-Mail';
-    }, 1200);
-});
+//     setTimeout(() => {
+//         emailBtnText.textContent = 'E-Mail';
+//     }, 1200);
+// });
 
-const container = document.getElementById('container');
-const btnPix = document.getElementById('btnPix');
-const pix = document.getElementById('pix');
+// const container = document.getElementById('container');
+// const btnPix = document.getElementById('btnPix');
+// const pix = document.getElementById('pix');
 
-btnPix.addEventListener('click', () => {
-    container.classList.add('hidden');
-    pix.classList.remove('hidden');
-});
+// btnPix.addEventListener('click', () => {
+//     container.classList.add('hidden');
+//     pix.classList.remove('hidden');
+// });
 
-const pixBack = document.getElementById('pixBack');
+// const pixBack = document.getElementById('pixBack');
 
-pixBack.addEventListener('click', () => {
-    container.classList.remove('hidden');
-    pix.classList.add('hidden');
-});
+// pixBack.addEventListener('click', () => {
+//     container.classList.remove('hidden');
+//     pix.classList.add('hidden');
+// });
 
-const pixBtn = document.getElementById('pixBtn');
-const pixKey = document.getElementById('pixKey');
+// const pixBtn = document.getElementById('pixBtn');
+// const pixKey = document.getElementById('pixKey');
 
-pixBtn.addEventListener('click', () => {
-    navigator.clipboard.writeText(pixKey.innerText);
+// pixBtn.addEventListener('click', () => {
+//     navigator.clipboard.writeText(pixKey.innerText);
 
-    pixBtn.textContent = 'Copiado!';
+//     pixBtn.textContent = 'Copiado!';
 
-    setTimeout(() => {
-        pixBtn.textContent = 'Copiar Chave Pix';
-    }, 1200);
-});
+//     setTimeout(() => {
+//         pixBtn.textContent = 'Copiar Chave Pix';
+//     }, 1200);
+// });
