3644545bab96e494b982b0615c733f5c706f0f4f
andregustavo1 <andregustavo5261@gmail.com>
2024-02-27 19:27:54 -0300

    up

---
+++ 000002/src/js/script.js
@@ -0,0 +1,29 @@
+const container = document.getElementById('container');
+const pixBtn = document.getElementById('pixBtn');
+const pixArea = document.getElementById('pixArea');
+
+pixBtn.addEventListener('click', () => {
+    container.classList.add('hidden');
+    pixArea.classList.remove('hidden');
+    window.scrollTo(0, 0);
+});
+
+const back = document.getElementById('back');
+
+back.addEventListener('click', () => {
+    container.classList.remove('hidden');
+    pixArea.classList.add('hidden');
+});
+
+const copyBtn = document.getElementById('copyBtn');
+const pixKey = document.getElementById('pixKey');
+
+copyBtn.addEventListener('click', () => {
+    navigator.clipboard.writeText(pixKey.innerText);
+
+    copyBtn.textContent = 'Copiado!';
+
+    setTimeout(() => {
+        copyBtn.textContent = 'Copiar chave PIX';
+    }, 1200);
+});
